1b7a3441bb6461328d747f86b08b710eeb84c026
Bilal El Haddadi <bilal.elhaddadi@student.vinci.be>
2022-11-11 19:04:34 +0100

    add : addPersons() in typescript

---
+++ exo1/typescript/src/components/App/App.tsx
@@ -0,0 +1,17 @@
+
+//import './App.css';
+import { AddPerson } from '../AddPerson/AddPerson';
+
+function App() {
+  return (
+    <div>
+
+        <AddPerson />
+
+    </div>
+
+
+  );
+}
+
+export default App;
