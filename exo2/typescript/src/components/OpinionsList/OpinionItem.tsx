19b0d7bb357f4386af221f0b0f4078a737f38971
Don Ismael <amine.nemmaoui@student.vinci.be>
2022-11-16 23:27:43 +0100

    refactoring exo2 -> ok

---
+++ exo2/typescript/src/components/OpinionsList/OpinionItem.tsx
@@ -0,0 +1,34 @@
+import React, { useContext } from 'react';
+import { OpinionContextType, Opinion } from "../../@types/todo";
+import opinionsContext from "../../contexts/opinionsContext";
+
+type Props = {
+    opinion: Opinion;
+}
+
+const OpinionItem: React.FC<Props> = ({opinion}) => {
+
+    const { voteForOpinion } = useContext(opinionsContext) as OpinionContextType;
+
+    const handleClick = () => {
+        voteForOpinion(opinion.id)
+    }
+
+    return (
+        <div>
+            <span className="label">
+                { opinion.label }
+            </span>
+            &nbsp;:&nbsp; 
+            <span className="score">
+                { opinion.score }
+            </span>
+            &nbsp;
+            <button onClick={handleClick}>
+                Vote
+            </button>
+        </div>
+    );
+}
+
+export default OpinionItem;
